4fb849cf3644658b58d179fb1109c08c0116779b
NicoloDiFazio <nico.uni.fisica@gmail.com>
2024-12-13 14:25:55 +0100

    second commit

--- analisi.py
+++ analisi.py
@@ -18,6 +18,7 @@ T  = np.array([])
 eT = np.array([])
 
 V_T  = np.array([])
+eV_T  = np.array([])
 
 file = open('out.dat','r')
 
@@ -32,12 +33,14 @@ for line in file:
         eI = np.append(eI,float(str[4]))
         
         T = np.append(T,(float(str[1])/R0 - 1)/A + 273.15)
+        eT = 
         #print(T)
 
 for i in range (0, n+1):
     V_T  = np.append(V_T, V[i]/T[i])
+    eV_T = np.append(eV_T,)
     
-gr = ROOT.TGraphErrors(len(V_T),V_T,I,eV,eI)
+gr = ROOT.TGraphErrors(len(V_T), V_T, I, eV_T, eI)
 
 
 f = ROOT.TF1("f", "[0]*(exp([1]*x)-1)", min, max)
@@ -47,7 +50,7 @@ f.SetParameter(1, 1.16e4)
 
 gr.Draw("AP")
 gr.Fit("f")
-print(f.GetParameter(1), f.GetParError(1))
-print("e/k ",1.6e-19/1.38e-23)
+print("e/k misurata: %f +- %f" %(f.GetParameter(1), f.GetParError(1)))
+print("e/k real: ", 1.6e-19/1.38e-23)
 f.Draw("SAME")
 ROOT.gApplication.Run(True)
