f8522c94b68e29576521a250216570f6d053830a
NicoloDiFazio <nico.uni.fisica@gmail.com>
2024-12-19 14:26:24 +0100

    twelfth commit

--- Millikan.py
+++ Millikan.py
@@ -2,11 +2,18 @@ import ROOT as R
 import math as m
 import numpy as np
 
-par =[6.7,1.6,0.30,0.28,0.35] #parametro di normalizzazione, media, sigma, Ampiezza1, Ampiezza2
+#par =[6.7,1.6,0.30,0.28,0.35] #parametro di normalizzazione, media, sigma, Ampiezza1, Ampiezza2
+p = 6.7
+mu = 1.6
+sig = 0.30
+a1 = 0.28
+a2 = 0.35
+
+par =[p,mu,sig,a1,a2] #parametro di normalizzazione, media, sigma, Ampiezza1, Ampiezza2
+pars =['p','mu','sig','a1','a2']
 #definisco funzione multigaussiana
 def multiGauss(x, par):
     # Funzione multi-gaussiana: somma di 3 gaussiane
-    # par contiene: [A1, mu1, sigma1, A2, mu2, sigma2, A3, mu3, sigma3]
     return par[0]*(par[3] * R.TMath.Gaus(x[0], par[1], par[2], 1) +
                    par[4] * R.TMath.Gaus(x[0], 2*par[1], par[2], 1) +
                    (1-par[4]-par[3]) * R.TMath.Gaus(x[0], 3*par[1], par[2], 1))
@@ -25,15 +32,17 @@ for value in dat:
     a.Fill(value) # Riempio istogramma con i dati
 
 #fit
-f = R.TF1("f", multiGauss, 0, 5.5, 5)
-for i in range (5):
+f = R.TF1("f", multiGauss, 0, 5.5, len(par))
+for i in range (len(par)):
     f.SetParameter(i, par[i])
     
 a.Draw()
 a.Fit("f","LR")
 
-for i in range (5):
-    print("parametro", i,":", f.GetParameter(i))
-    
+for i in range (len(par)):
+    print(pars[i],":", par[i])
+    print(pars[i],"fit:", f.GetParameter(i))
+
+print("e = %f" %(f.GetParameter(1)))
 R.gApplication.Run(True)
 
